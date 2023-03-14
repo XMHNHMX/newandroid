8b0d9ba3351f7ff29e9b5b15a4609b602d85fc25
MAX.MIHIN <34857184+carboraneacid@users.noreply.github.com>
2023-03-14 23:46:28 +0300

    Update on.py

--- on.py
+++ on.py
@@ -45,63 +45,50 @@ def append(self, symbol):
 
 # Определяем метод calculate для вычисления результата введенного выражения
 
+
+# Определяем метод calculate для вычисления результата введенного выражения
 def calculate(self):
-
     try:
-
+        # Проверяем длину и содержание выражения 
+        if len(self.input) == 0 or all(char in ".+-*/" for char in self.input):
+            # Генерируем исключение ValueError с сообщением "Empty or invalid expression"
+            raise ValueError("Empty or invalid expression")
+        
         # Пытаемся разделить выражение на два операнда и оператор по пробелу 
-
         operand1, operator, operand2 = self.input.split()
-
+        
+        # Проверяем каждый символ в операндах на то, что он является цифрой или точкой 
+        if not all(char.isdigit() or char == "." for char in operand1) or not all(char.isdigit() or char == "." for char in operand2):
+            # Генерируем исключение ValueError с сообщением "Invalid operands"
+            raise ValueError("Invalid operands")
+        
         # Преобразуем операнды в числа с плавающей запятой 
-
         operand1 = float(operand1)
-
         operand2 = float(operand2)
-
+        
         # Выполняем математическое действие в зависимости от оператора и присваиваем результат атрибуту output 
-
         if operator == "+":
-
             self.output = operand1 + operand2
-
         elif operator == "-":
-
             self.output = operand1 - operand2
-
         elif operator == "*":
-
             self.output = operand1 * operand2
-
         elif operator == "/":
-
             self.output = operand1 / operand2
-
         else:
-
             raise ValueError("Invalid operator")
-
         
-
         # Если результат - бесконечность или не число, генерируем исключение ValueError 
-
         if math.isinf(self.output) or math.isnan(self.output):
-
             raise ValueError("Invalid result")
-
         
-
         # Форматируем результат с двумя знаками после запятой 
-
         self.output = f"{self.output:.2f}"
-
     
-
     except (ValueError) as e:
-
          # Если возникло исключение при разборе или выполнении выражения, присваиваем сообщение об ошибке атрибуту error 
-
          self.error = f"Error: {e}"
+
 
 # Определяем метод display для отображения состояния калькулятора
 
@@ -124,66 +111,4 @@ def display(self):
 # Создаем объект calculator из класса Calculator 
 calculator = Calculator()
 
-# Тестируем работу калькулятора с разными примерами 
 
-# Пример 1: 2 + 3 
-calculator.append("2")
-calculator.append("+")
-calculator.append("3")
-calculator.calculate()
-calculator.display()
-# Output: Input: 2+3 Output: 5.00
-
-# Пример 2: 7 - 4 
-calculator.clear()
-calculator.append("7")
-calculator.append("-")
-calculator.append("4")
-calculator.calculate()
-calculator.display()
-# Output: Input: 7-4 Output: 3.00
-
-# Пример 3: 6 * 8 
-calculator.clear()
-calculator.append("6")
-calculator.append("*")
-calculator.append("8")
-calculator.calculate()
-calculator.display()
-# Output: Input: 6*8 Output: 48.00
-
-# Пример 4: 9 / 3 
-calculator.clear()
-calculator.append("9")
-calculator.append("/")
-calculator.append("3")
-calculator.calculate()
-calculator.display()
-# Output: Input: 9/3 Output: 3.00
-
-# Пример 5: . + .
-calculator.clear()
-calculator.append(".")
-calculator.append("+")
-calculator.append(".")
-calculator.calculate()
-calculator.display()
-# Output: Error: invalid syntax
-
-# Пример 6: 5 + . 
-calculator.clear()
-calculator.append("5")
-calculator.append("+")
-calculator.append(".")
-calculator.calculate()
-calculator.display()
-# Output: Error: invalid syntax
-
-# Пример 7: 9 / 0 
-calculator.clear()
-calculator.append("9")
-calculator.append("/")
-calculator.append("0")
-calculator.calculate()
-calculator.display()
-# Output: Error: division by zero
